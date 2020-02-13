ee50d327f27d86db79731dbbb2aca98eaf3e0918
AndyJung <andy.mytutor@gmail.com>
2020-02-13 16:41:44 +0800

    schedule 未成功

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Str;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+
     ];
 
     /**
@@ -24,8 +26,22 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            $name= 'a'.random_int(1000, 9999999);
+            $email = $name.'@app.com';
+            User::create([
+                'name' => $name,
+                'email' => $email,
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'avatar' => '',
+                'birthday' => null,
+                'introduction' => 'aaa',
+            ]);
+        })->everyMinute();
+
+
     }
 
     /**
