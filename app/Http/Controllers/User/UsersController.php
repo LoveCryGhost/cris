2159917bb818e1f38ff59a17b9ab1c3f897f3c9f
AndyJung <andy.mytutor@gmail.com>
2020-02-13 15:31:38 +0800

    User 授權訪問

--- app/Http/Controllers/User/UsersController.php
+++ app/Http/Controllers/User/UsersController.php
@@ -11,15 +11,22 @@ use App\Models\User;
 class UsersController extends UserCoreController
 {
 
-    //顯示使用者資料
-    public function show(User $user)
+    public function __construct()
     {
+        $this->middleware('auth');
+    }
+
+
+    //顯示使用者資料
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
         return view(config('theme.user.view').'user.show', compact('user'));
     }
 
     public function update(UserRequest $request,  ImageUploadHandler $uploader,User $user)
     {
-
+        $this->authorize('update', $user);
         $data = $request->all();
         if($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
