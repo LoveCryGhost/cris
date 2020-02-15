d4cf30c4c4f179dccab4b66743ffd1b3575d491b
AndyJung <andy.mytutor@gmail.com>
2020-02-16 02:55:34 +0800

    Product相關 ProductDataTable

---
+++ app/Models/ProductThumbnail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;;
+
+class Product extends Model
+{
+
+    protected $table = "products";
+    protected $primaryKey='p_id';
+
+    protected $fillable = [
+        'publish_at',
+        't_id',
+        'name',
+        'c_id',
+    ];
+
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'products_categories','p_id','c_id')
+            ->withTimestamps();
+    }
+
+}
