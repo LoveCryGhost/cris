f766d76c3b74ca86ce68c3e3f7fe7290396f32e3
AndyJung <andy.mytutor@gmail.com>
2020-02-15 17:16:40 +0800

    composer require jenssegers/agent

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\LoginSuccessfulEvent;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Providers\RouteServiceProvider;
