1372b5e77c24e5e63a3561e05261021bc4e82f63
AndyJung <andy.mytutor@gmail.com>
2020-02-20 22:25:11 +0800

    create suppier contacts database

---
+++ database/migrations/2020_02_20_090100_create_supplier_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupplierContactsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('supplier_contacts', function (Blueprint $table) {
+            $table->bigIncrements('sc_id');
+            $table->bigInteger('s_id')->unsigned();
+            $table->string('id_code')->unique()->nullable();
+            $table->string('sc_name');
+            $table->string('name_card')->nullable();
+            $table->string('tel')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->bigInteger('member_id')->unsigned();
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+            $table->foreign('s_id')->references('s_id')->on('suppliers')->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+
+
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('supplier_contacts');
+    }
+}
---
+++ database/seeds/SuppliersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Handlers\BarcodeHandler;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class SuppliersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $suppliers = [
+            [],
+            []
+        ];
+    }
+}
