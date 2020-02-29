1b1d10573d427919acf95552caf5963149e9331b
AndyJung <andy.mytutor@gmail.com>
2020-02-29 15:20:38 +0800

    優化Product

--- resources/views/theme/cryptoadmin/member/product/edit.blade.php
+++ resources/views/theme/cryptoadmin/member/product/edit.blade.php
@@ -112,24 +112,14 @@
 
 @section('js')
     @parent
-
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js"></script>
-    <link href="https://raw.githack.com/ttskch/select2-bootstrap4-theme/master/dist/select2-bootstrap4.css" rel="stylesheet">
     <script type="text/javascript">
         $(function(){
             //Select2
-            select2_item =  $('.select2_item');
-            select2_item.select2({
-                theme: "bootstrap4"
-            });
-
+            active_select2(select2_class='select2_item', options={});
             //Switch
-            $bt_switch = $('.bt-switch');
-            $bt_switch.bootstrapSwitch('toggleState', true);
+            active_switch(switch_class='bt-switch', options=[]);
         })
     </script>
-
 @endsection
 
 
